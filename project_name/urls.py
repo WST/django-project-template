a44b3b2bd80162ebf2a16d97a4aa4f161393243f
WST <ilya.averkov@gmail.com>
2015-11-27 13:25:21 +0300

    пара правок

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# Внимание! Это глобальный urls.py проекта. Добавлять свои URL-ки лучше в urls.py приложения «portal».
+
 # Django
 from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
 
 # Наш проект
@@ -9,5 +12,9 @@ from portal import urls as portal_urls
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^captcha/', include('captcha.urls')),
 	url(r'', include(portal_urls)),
+
+	# Чисто отладочная строка
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 ]
--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for project_name project.
+# -*- coding: utf-8 -*-
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# WSGI-файл для развёртывания
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
+# Системные импорты
 import os
 
+# Django
 from django.core.wsgi import get_wsgi_application
 
+# Задаём путь к модулю настроек
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
 
+# Наш WSGI callable
 application = get_wsgi_application()
